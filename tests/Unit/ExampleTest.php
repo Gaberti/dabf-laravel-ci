e3ade22ff23f7a674b86a3d4dfff800837ac62aa
gaberti <gberti@DESKTOP-AOK48F5.>
2024-10-21 22:34:42 -0300

    new tests and asserts v1

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,7 +2,10 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use Tests\TestCase;
+use App\Models\User;
+
+
 
 class ExampleTest extends TestCase
 {
@@ -12,5 +15,44 @@ class ExampleTest extends TestCase
     public function test_that_true_is_true(): void
     {
         $this->assertTrue(true);
+    }
+
+    public function test_create_user(): void
+    {
+        $user = User::create([
+            'name' => 'test123',
+            'email' => 'test123@mail.com'
+        ]);
+
+        $this->assertNotNull($user->name);
+
+        $this->assertEquals('test123', $user->name);
+    }
+
+    public function test_exists_user(): void
+    {
+        User::create([
+            'name' => 'test1234',
+            'email' => 'test1234@mail.com'
+        ]);
+
+        $this->assertDatabaseHas('users', [
+            'email' => 'test1234@mail.com',
+            'name' => 'test1234',
+        ]);
+    }
+
+    public function test_user_name_update()
+    {
+        $user = User::create([
+            'name' => 'user-test',
+            'email' => 'user@mail.com',
+            'password' => bcrypt('pass123'),
+        ]);
+
+        $user->name = 'updt-user';
+        $user->save();
+
+        $this->assertEquals('updt-user', $user->name);
     }
 }
